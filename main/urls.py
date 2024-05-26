09c310643f4c0e9d31fafafa20a2fef9cb06f5ba
Sentimentallkillthemall <matveypotapenko112@gmail.com>
2024-05-27 00:35:50 +0300

    commit_1

--- main/urls.py
+++ main/urls.py
@@ -5,8 +5,11 @@ from . import views
 urlpatterns = [
     path('', views.index, name='home'),
     path('create', views.create_vacancy, name='create'),
+    path('createsummary', views.create_summary, name='create-summary'),
     path('summary', views.summary_list, name='summary'),
     path('update/<int:vacancy_id>/', views.update_vacancy, name='update-vacancy'),
-    path('delete/<int:vacancy_id>/', views.delete_vacancy, name='delete-vacancy')
+    path('updatesummary/<int:summary_id>/', views.update_summary, name='update-summary'),
+    path('delete/<int:vacancy_id>/', views.delete_vacancy, name='delete-vacancy'),
+    path('deletesummary/<int:summary_id>/', views.delete_summary, name='delete-summary')
 
 ]
--- main/views.py
+++ main/views.py
@@ -29,36 +29,41 @@ def update_vacancy(request, vacancy_id):
         'error': error
     }
     return render(request, 'main/update_vacancy.html', context)
-
-def delete_vacancy(requset, vacancy_id):
+def update_summary(request, summary_id):
     error = ''
-    vacancy_to_delete = Vacancy.objects.get(id=vacancy_id)
-    vacancy_to_delete.delete()
-    form = VacancyForm(instance=vacancy_to_delete)
+    summary_to_edit = Summary.objects.get(id=summary_id)
+    if request.method == 'POST':
+        form = SummaryForm(request.POST, instance=summary_to_edit)
+        if summary_to_edit.author and request.user.id == summary_to_edit.author.id:
+            if form.is_valid():
+                post = form.save(commit=False)
+                post.author = request.user
+                post.save()
+                return redirect('home')
+            else:
+                error = 'Форма была неверной'
+        else:
+            error = 'Вы не можете изменить это резюме, так как вы не её автор'
+    else:
+        form = SummaryForm(instance=summary_to_edit)
     context = {
         'form': form,
         'error': error
     }
-    return render( 'delete_vacancy.html', context)
-    # if request.method == 'POST':
-    #     form = VacancyForm(request.POST, instance=vacancy_to_delete)
-    #     if vacancy_to_delete.author and request.user.id == vacancy_to_delete.author.id:
-    #         if form.is_valid():
-    #             post = form.save(commit=False)
-    #             post.author = request.user
-    #             post.save()
-    #             return redirect('home')
-    #         else:
-    #             error = 'Форма была неверной'
-    #     else:
-    #         error = 'Вы не можете удалить эту вакансию, так как вы не её автор'
-    # else:
-    #     form = VacancyForm(instance=vacancy_to_delete)
-    # context = {
-    #     'form': form,
-    #     'error': error
-    # }
-    # return render(request, 'main/delete_vacancy.html', context)
+    return render(request, 'main/update_summary.html', context)
+
+
+def delete_vacancy(requset, vacancy_id):
+    vacancy_to_delete = Vacancy.objects.get(id=vacancy_id)
+    vacancy_to_delete.delete()
+
+    return redirect('home')
+def delete_summary(requset, summary_id):
+    summary_to_delete = Summary.objects.get(id=summary_id)
+    summary_to_delete.delete()
+
+    return redirect('summary')
+
 
 def summary_list(request):
     summaries = Summary.objects.all()
@@ -84,4 +89,24 @@ def create_vacancy(request):#не пон
         'error': error
     }
     return render(request, 'main/create.html', context)
+def create_summary(request):#не пон
+    error = ''
+    if request.method == 'POST':
+        form = SummaryForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect('home')
+        else:
+            error = 'Форма была неверной'
+
+
+    else:
+        form = SummaryForm()
+    context = {
+        'form': form,
+        'error': error
+    }
+    return render(request, 'main/create_summary.html', context)
 
